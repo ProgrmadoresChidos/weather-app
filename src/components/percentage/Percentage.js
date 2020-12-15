334f25445b037050cd86126d0cab8e83ce4ab33d
Roberto Valdez <robert.tito94@gmail.com>
2020-12-15 16:51:57 -0600

    clean code

--- src/components/percentage/Percentage.js
+++ src/components/percentage/Percentage.js
@@ -6,13 +6,11 @@ export const Percentage = ({ percentage }) => {
     return (
         <div>
             <div className={style.styleNumbers}>0</div>
-            <div className={style.styleNumbers, style.positionFifty}>50</div>
-            <div className={style.stylenumbers, style.positionFifty100}>100</div>
+            <div className={`${style.styleNumbers} ${style.positionFifty}`}>50</div>
+            <div className={`${style.stylenumbers} ${style.positionFifty100}`}>100</div>
             <div className={style.whiteRectangle}>
-
-
                 <div className={style.yellowRectangle} style={{
-                    width: (percentage*100)/229
+                    width: (percentage * 100) / 229
                 }}>
                 </div>
             </div>
