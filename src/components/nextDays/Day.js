7963c3bf4c9f28535c6e8ad4a1f84a1c8e24d579
Nestor <nestor_gm02@hotmail.com>
2020-12-15 15:49:46 -0600

    estilo agregado a Day

--- src/components/nextDays/Day.js
+++ src/components/nextDays/Day.js
@@ -4,10 +4,10 @@ export const Day = ({ date, weather, maxTemp, minTemp }) => {
   return (
     <div className={style.day}>
       <p>{date}</p>
-      <p>{weather}</p>
+      <img src={`https://www.metaweather.com/static/img/weather/png/64/${weather}.png`} alt="weather" />
       <div className={style.tempWrapper}>
         <p>{maxTemp}°C</p>
-        <p>{minTemp}°C</p>
+        <p className={style.minTemp}>{minTemp}°C</p>
       </div>
     </div>
   )
--- src/components/nextDays/NextDays.js
+++ src/components/nextDays/NextDays.js
@@ -4,11 +4,11 @@ import { Day } from './Day';
 export const NextDays = () => {
     return (
         <section className={style.nextDays}>
-            <Day className={style.day} date={'tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
-            <Day className={style.day} date={'tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
-            <Day className={style.day} date={'tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
-            <Day className={style.day} date={'tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
-            <Day className={style.day} date={'tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
+            <Day className={style.day} date={'Tomorrow'} weather={'sn'} maxTemp={16} minTemp={11} />
+            <Day className={style.day} date={'Sun, 7 Jun'} weather={'sl'} maxTemp={16} minTemp={11} />
+            <Day className={style.day} date={'Mon, 8 Jun'} weather={'h'} maxTemp={16} minTemp={11} />
+            <Day className={style.day} date={'Tue, 9 Jun'} weather={'t'} maxTemp={16} minTemp={11} />
+            <Day className={style.day} date={'Wed, 10 Jun'} weather={'hr'} maxTemp={16} minTemp={11} />
         </section>
     )
 }
