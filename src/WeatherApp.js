b48a5cab6553a2912160ec009052d9e337c8ff4d
Roberto Valdez <robert.tito94@gmail.com>
2020-12-15 19:01:56 -0600

    acomodar elementos

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import style from './WeatherApp.module.css';
 import { Highlight } from './components/highlight/Highlight'
 import { NextDays } from './components/nextDays/NextDays'
 
 export const WeatherApp = () => {
     return (
-        <div>
-            Weather works!!!
-            <NextDays />
-            <Highlight />
+        <div className={style.weatherApp}>
+            {/* <div className={style.weatherApp__container}>
+            </div> */}
+                <div className={style.weatherApp__sidebar}>
+                    component
+            </div>
+                <div className={style.weatherApp__content}>
+                    <NextDays />
+                    <Highlight />
+                </div>
         </div>
     )
 }
